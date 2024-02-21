3f41e8c9b2c20ddf37189b6a643ba944906e50bf
Cassiano Castro <development.castro@gmail.com>
2024-02-21 14:56:41 -0300

    Style: indent adjust

--- public/html/home.php
+++ public/html/home.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-BR" dir="ltr">
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="icon" type="image/x-icon" size="24x24" href="src/public/favicons/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="src/public/css/table.css">
+  <link rel="stylesheet" type="text/css" href="src/public/css/table.css">
   <link rel="manifest" href="./manifest.json">
 
   <title>API | Home</title>
 </head>
 <body>
-	<?php include_once __DIR__ . '/includes/header.inc'; ?>
+  <?php include_once __DIR__ . '/includes/header.inc'; ?>
 
-	<main>
+  <main>
 
     <section>
       <h2>Routes</h2>
@@ -81,8 +81,8 @@
 
     </section>
 
-	</main>
+  </main>
 
-	<?php include_once __DIR__ . '/includes/footer.inc'; ?>
+  <?php include_once __DIR__ . '/includes/footer.inc'; ?>
 </body>
 </html>
--- public/html/logout.php
+++ public/html/logout.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-BR" dir="ltr">
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>API | Logout</title>
 </head>
 <body>
-	<?php include_once __DIR__ . './includes/header.inc'; ?>
+  <?php include_once __DIR__ . './includes/header.inc'; ?>
 
-	<main>
+  <main>
 
-		<noscript>
-			<p>Enable the JavaScript in your browser.</p>
-		</noscript>
+    <noscript>
+      <p>Enable the JavaScript in your browser.</p>
+    </noscript>
 
-		<section>
-			<h2></h2>
+    <section>
+      <h2></h2>
 
-		</section>
+    </section>
 
-	</main>
+  </main>
 
-	<?php include_once __DIR__ . './includes/footer.inc'; ?>
+  <?php include_once __DIR__ . './includes/footer.inc'; ?>
 
-	<script src=""></script>
+  <script src=""></script>
 </body>
 </html>
