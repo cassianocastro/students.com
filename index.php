c0f39f6969288c998c32e56a731f59496e7349d9
Cassiano Castro <development.castro@gmail.com>
2024-02-23 18:13:56 -0300

    Created instance of HomeController

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * @test
@@ -27,7 +28,10 @@ function canAccessApiWithAuthorization(string $user, string $password): void
  */
 function index()
 {
-    // canAccessApiWithAuthorization("", "");
+    error_reporting(E_ALL);
+    ini_set("display_errors", true);
+
+    (new App\controller\HomeController())->index();
 }
 
 index();
