b6464e18e290b06d27d078ef84ea1181a80cc757
Cassiano Castro <development.castro@gmail.com>
2024-02-23 18:25:51 -0300

    Restructured HTML

--- public/html/logout.php
+++ public/html/logout.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="pt-BR" dir="ltr">
+<html lang="en-US" dir="ltr">
 <head>
+  <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>API | Logout</title>
@@ -10,21 +10,19 @@
 <body>
   <?php require_once __DIR__ . './includes/header.inc'; ?>
 
+  <noscript>
+    <p>
+      Enable the JavaScript in your browser.
+    </p>
+  </noscript>
+
   <main>
-
-    <noscript>
-      <p>Enable the JavaScript in your browser.</p>
-    </noscript>
-
     <section>
       <h2></h2>
 
     </section>
-
   </main>
 
   <?php require_once __DIR__ . './includes/footer.inc'; ?>
-
-  <script src=""></script>
 </body>
 </html>
