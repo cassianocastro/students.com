e226258b2bfc55ef42855e8945c9bdc72d54efe9
Cassiano Castro <development.castro@gmail.com>
2024-02-23 17:40:43 -0300

    Files addition

---
+++ app/view/HTMLDocument.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Api\Views;
+
+/**
+ *
+ */
+final class HTMLDocument implements Document
+{
+
+    private string $path;
+    private mixed $params;
+
+    public function __construct(string $path, mixed $params = NULL)
+    {
+        $this->path   = __DIR__ . "/../../public/html/$path";
+        $this->params = $params;
+    }
+
+    public function getContent(): string
+    {
+        ob_start();
+
+        require_once $this->path;
+
+		$content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+}
