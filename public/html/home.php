60240be477996294a1638dfb8ccba0426ba6acfd
Cassiano Castro <development.castro@gmail.com>
2024-02-23 18:19:21 -0300

    Fix: .css file not found
    Updated value of href attribute on HTML link element

--- public/html/home.php
+++ public/html/home.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" type="text/css" href="src/public/css/table.css">
-  <link rel="manifest" href="./manifest.json">
+  <link rel="stylesheet" type="text/css" href="public/css/table.css">
+  <link rel="manifest" type="application/json" href="./manifest.json">
 
   <title>API | Home</title>
 </head>
