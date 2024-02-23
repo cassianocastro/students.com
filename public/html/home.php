b758e51c4adcb95963b86ab4a75e6ce1b3cc417d
Cassiano Castro <development.castro@gmail.com>
2024-02-23 18:20:54 -0300

    Changed language's document

--- public/html/home.php
+++ public/html/home.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="pt-BR" dir="ltr">
+<html lang="en-US" dir="ltr">
 <head>
+  <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="stylesheet" type="text/css" href="public/css/table.css">
